a6bc3ddfa2043051a8bbf1a77f246a80d7fd06d8
Summer <Summerrichards01@gmail.com>
2019-11-02 17:01:32 -0500

    creating full-name component

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatIconModule, MatMenuModule } from '@angular/material';
+import { MatButtonModule, MatIconModule, MatMenuModule, MatFormFieldModule } from '@angular/material';
 import { MatRadioModule } from '@angular/material/radio';
 import { AppointmentPageComponent } from './appointment-page/appointment-page.component';
 import { IntroToAppFormComponent } from './appointment-page/intro-to-app-form/intro-to-app-form.component';
 import { NameOfPatientComponent } from './appointment-page/name-of-patient/name-of-patient.component';
 import { HomeButtonColorComponent } from './appointment-page/home-button-color/home-button-color.component';
+import { FullNameComponent } from './appointment-page/full-name/full-name.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -21,8 +23,9 @@ import { HomeButtonColorComponent } from './appointment-page/home-button-color/h
     AppointmentPageComponent,
     IntroToAppFormComponent,
     NameOfPatientComponent,
-    HomeButtonColorComponent
-  ],
+    HomeButtonColorComponent,
+    FullNameComponent
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -32,7 +35,9 @@ import { HomeButtonColorComponent } from './appointment-page/home-button-color/h
     MatButtonModule,
     MatMenuModule,
     MatRadioModule,
-
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]

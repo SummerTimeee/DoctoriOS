6498603b98174d2a5ba0a4b5644d7e8d44a5f5a7
Summer <Summerrichards01@gmail.com>
2019-11-04 10:54:27 -0600

    adding MatInputModule

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatIconModule, MatMenuModule, MatFormFieldModule } from '@angular/material';
+import { MatButtonModule, MatIconModule, MatMenuModule, MatFormFieldModule, MatInputModule } from '@angular/material';
 import { MatRadioModule } from '@angular/material/radio';
 import { AppointmentPageComponent } from './appointment-page/appointment-page.component';
 import { IntroToAppFormComponent } from './appointment-page/intro-to-app-form/intro-to-app-form.component';
@@ -45,7 +45,8 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
     MatRadioModule,
     ReactiveFormsModule,
     MatFormFieldModule,
-    FormsModule
+    FormsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]

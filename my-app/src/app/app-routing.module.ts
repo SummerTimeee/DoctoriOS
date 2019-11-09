d0cb721f5901e29e3f6352e0211965a1b6423256
Summer <Summerrichards01@gmail.com>
2019-11-08 20:55:02 -0600

    removing divs adding hoes

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -3,15 +3,10 @@ import {Routes, RouterModule} from '@angular/router';
 import {BlogComponent} from './blog/blog.component';
 import { HomeComponent } from './home/home.component';
 
-
 const routes: Routes =
   [
-    {
-      path: '', component: HomeComponent
-    },
-    {
-      path: 'blog',  component: BlogComponent
-    }
+    {path: '', component: HomeComponent},
+    {path: 'blog',  component: BlogComponent}
   ];
 
 @NgModule({

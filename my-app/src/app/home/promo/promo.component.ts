30a02320cabd143c33e6521e3b11e0b3a501d95c
Summer <Summerrichards01@gmail.com>
2020-03-27 14:22:05 -0500

    adding animation to promo component

--- my-app/src/app/home/promo/promo.component.ts
+++ my-app/src/app/home/promo/promo.component.ts
@@ -5,7 +5,25 @@ import { Component } from '@angular/core';
   templateUrl: './promo.component.html',
   styleUrls: ['./promo.component.css']
 })
+
 export class PromoComponent {
-title = 'Deal Of The Month';
+  title = 'Deal Of The Month';
+
+  ngOnInit() {
+
+    var pos = 0;
+    var box = document.getElementById("box");
+    var t = setInterval(move, 10);
+
+    function move() {
+      if (pos >= 150) {
+        clearInterval(t)
+      }
+      else {
+        pos += 1;
+        box.style.left = pos + 'px';
+      }
+    }
+  }
 
 }

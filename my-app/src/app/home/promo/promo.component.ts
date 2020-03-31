0e6aa07aa1f2ae945625a634b01303781c4c5f71
Summer <Summerrichards01@gmail.com>
2020-03-31 12:42:02 -0500

    chaning countdown timer

--- my-app/src/app/home/promo/promo.component.ts
+++ my-app/src/app/home/promo/promo.component.ts
@@ -9,21 +9,21 @@ import { Component } from '@angular/core';
 export class PromoComponent {
   title = 'Deal Of The Month';
 
-  ngOnInit() {
+  // ngOnInit() {
 
-    var pos = 0;
-    var box = document.getElementById("box");
-    var t = setInterval(move, 10);
+  //   var pos = 0;
+  //   var slide = document.getElementById("slide");
+  //   var t = setInterval(move, 3);
 
-    function move() {
-      if (pos >= 150) {
-        clearInterval(t)
-      }
-      else {
-        pos += 1;
-        box.style.left = pos + 'px';
-      }
-    }
-  }
+  //   function move() {
+  //     if (pos >= 450) {
+  //       clearInterval(t)
+  //     }
+  //     else {
+  //       pos += 1;
+  //       slide.style.left = pos + 'px';
+  //     }
+  //   }
+  // }
 
 }

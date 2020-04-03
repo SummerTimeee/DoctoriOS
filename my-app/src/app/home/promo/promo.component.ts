27f5c0f09c3c967f7278e07e63a439c2d4ae04f0
Summer <Summerrichards01@gmail.com>
2020-04-03 12:31:29 -0500

    adding animation to home page

--- my-app/src/app/home/promo/promo.component.ts
+++ my-app/src/app/home/promo/promo.component.ts
@@ -9,21 +9,27 @@ import { Component } from '@angular/core';
 export class PromoComponent {
   title = 'Deal Of The Month';
 
-  // ngOnInit() {
+  ngOnInit() {
 
-  //   var pos = 0;
-  //   var slide = document.getElementById("slide");
-  //   var t = setInterval(move, 3);
+    var pos = 0;
+    var slide = document.getElementById("slide");
+    var t = setInterval(move, 8);
 
-  //   function move() {
-  //     if (pos >= 450) {
-  //       clearInterval(t)
-  //     }
-  //     else {
-  //       pos += 1;
-  //       slide.style.left = pos + 'px';
-  //     }
-  //   }
-  // }
-
+    function move() {
+      if (screen && screen.width > 1000 && pos >= 450 ) {
+        clearInterval(t)
+      }
+      else {
+        pos += 1;
+        slide.style.left = pos + 'px';
+      }
+      if (screen && screen.width < 500 && pos >= 120) {
+        clearInterval(t)
+      }
+      else {
+        pos += 1;
+        slide.style.left = pos + 'px'
+      }
+    }
+  }
 }

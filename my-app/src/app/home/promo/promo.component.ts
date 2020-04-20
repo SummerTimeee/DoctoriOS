5536fb49b3a8d66d7cdc205d3022e4aa7ccaa321
Summer <Summerrichards01@gmail.com>
2020-04-20 17:22:19 -0500

    removing animations

--- my-app/src/app/home/promo/promo.component.ts
+++ my-app/src/app/home/promo/promo.component.ts
@@ -11,25 +11,25 @@ export class PromoComponent {
 
   ngOnInit() {
 
-    var pos = 0;
-    var slide = document.getElementById("slide");
-    var t = setInterval(move, 8);
+    // var pos = 0;
+    // var slide = document.getElementById("slide");
+    // var t = setInterval(move, 8);
 
-    function move() {
-      if (screen && screen.width > 1000 && pos >= 450 ) {
-        clearInterval(t)
-      }
-      else {
-        pos += 1;
-        slide.style.left = pos + 'px';
-      }
-      if (screen && screen.width < 500 && pos >= 90) {
-        clearInterval(t)
-      }
-      else {
-        pos += 1;
-        slide.style.left = pos + 'px'
-      }
-    }
+    // function move() {
+    //   if (screen && screen.width > 1000 && pos >= 450 ) {
+    //     clearInterval(t)
+    //   }
+    //   else {
+    //     pos += 1;
+    //     slide.style.left = pos + 'px';
+    //   }
+    //   if (screen && screen.width < 500 && pos >= 90) {
+    //     clearInterval(t)
+    //   }
+    //   else {
+    //     pos += 1;
+    //     slide.style.left = pos + 'px'
+    //   }
+    // }
   }
 }

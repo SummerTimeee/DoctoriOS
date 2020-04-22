e7e32fd22008d98b4932640fca14d34981696cd1
Summer <Summerrichards01@gmail.com>
2020-04-22 16:44:22 -0500

    creating animation for home page

--- my-app/src/app/home/promo/promo.component.ts
+++ my-app/src/app/home/promo/promo.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'promo',
@@ -6,30 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./promo.component.css']
 })
 
-export class PromoComponent {
+export class PromoComponent implements OnInit {
   title = 'Deal Of The Month';
+
 
   ngOnInit() {
 
-    // var pos = 0;
-    // var slide = document.getElementById("slide");
-    // var t = setInterval(move, 8);
+    var text = this.title;
+    var splitText= text.split('');
 
-    // function move() {
-    //   if (screen && screen.width > 1000 && pos >= 450 ) {
-    //     clearInterval(t)
-    //   }
-    //   else {
-    //     pos += 1;
-    //     slide.style.left = pos + 'px';
-    //   }
-    //   if (screen && screen.width < 500 && pos >= 90) {
-    //     clearInterval(t)
-    //   }
-    //   else {
-    //     pos += 1;
-    //     slide.style.left = pos + 'px'
-    //   }
-    // }
+    console.log(splitText);
   }
-}
+};

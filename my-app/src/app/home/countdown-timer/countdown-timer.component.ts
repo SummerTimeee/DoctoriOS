93196e6d560bd49f79146ab10d800ba31e048188
Summer <Summerrichards01@gmail.com>
2020-05-01 17:14:32 -0500

    editing countdown timer

--- my-app/src/app/home/countdown-timer/countdown-timer.component.ts
+++ my-app/src/app/home/countdown-timer/countdown-timer.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./countdown-timer.component.css']
 })
 export class CountdownTimerComponent implements OnInit {
-  countDown = new Date('April 30, 2020 06:00:25').getTime();
+  countDown = new Date('May 31, 2020 12:00:00').getTime();
   timer = '';
   title = 'Hurry! Sale Ends:'
 
--- my-app/src/app/services/cell-phone-patients/cell-phone-patients.component.ts
+++ my-app/src/app/services/cell-phone-patients/cell-phone-patients.component.ts
@@ -6,35 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./cell-phone-patients.component.css']
 })
 export class CellPhonePatientsComponent {
-  phones: any[] = [
-    {
-      "phoneName": "iPhone 8+",
-      "repairPrice": `$${75.00}`,
-      "oldRepairPrice": `$${90.00}`,
-      "image": "assets/images/logo.png"
-    },
-    {
-
-      "phoneName": "iPhone 8",
-      "repairPrice": `$${70.00}`,
-      "oldRepairPrice": `$${85.00}`,
-      "image": "url(https://store.storeimages.cdn-apple.com/4981/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone8/plus/iphone8-plus-spgray-select-2018?wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1522347735544)"
-    },
-    {
-      "phoneName": "iPhone 7+",
-      "repairPrice": `$${75.00}`,
-      "oldRepairPrice":`$${90.00}`,
-      "image": "url(https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone7/plus/iphone7-plus-silver-select-2016?wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1472430173129)"
-
-    }
-
-
-
-
-
-
-
-  ];
 
 }
 

f36f75f2ceb790f60e54627d4ecc52e5d34a6bdf
Summer <Summerrichards01@gmail.com>
2020-04-30 23:59:05 -0500

    adding array of ojects in cell-phone-patients.ts file

--- my-app/src/app/services/cell-phone-patients/cell-phone-patients.component.ts
+++ my-app/src/app/services/cell-phone-patients/cell-phone-patients.component.ts
@@ -8,23 +8,23 @@ import { Component } from '@angular/core';
 export class CellPhonePatientsComponent {
   phones: any[] = [
     {
-      "phoneName": 'iPhone 8+',
+      "phoneName": "iPhone 8+",
       "repairPrice": `$${75.00}`,
       "oldRepairPrice": `$${90.00}`,
-      "image": 'url(https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone8/plus/iphone8-plus-gold-select-2018?wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1522347727972)'
+      "image": "assets/images/logo.png"
     },
     {
 
-      "phoneName": 'iPhone 8',
+      "phoneName": "iPhone 8",
       "repairPrice": `$${70.00}`,
       "oldRepairPrice": `$${85.00}`,
-      "image": 'url(https://store.storeimages.cdn-apple.com/4981/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone8/plus/iphone8-plus-spgray-select-2018?wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1522347735544)'
+      "image": "url(https://store.storeimages.cdn-apple.com/4981/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone8/plus/iphone8-plus-spgray-select-2018?wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1522347735544)"
     },
     {
-      "phoneName": 'iPhone 7+',
+      "phoneName": "iPhone 7+",
       "repairPrice": `$${75.00}`,
       "oldRepairPrice":`$${90.00}`,
-      "image": 'url(https://image.shutterstock.com/image-photo/peach-isolated-on-white-full-260nw-669280930.jpg)'
+      "image": "url(https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone7/plus/iphone7-plus-silver-select-2016?wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1472430173129)"
 
     }
 

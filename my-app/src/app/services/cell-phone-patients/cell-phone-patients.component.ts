83db568d1365dd55f1b9bc19922dc80a841ed6f8
Summer <Summerrichards01@gmail.com>
2020-05-18 11:56:10 -0500

    adding new cell phone patient to cell phone component

--- my-app/src/app/services/cell-phone-patients/cell-phone-patients.component.ts
+++ my-app/src/app/services/cell-phone-patients/cell-phone-patients.component.ts
@@ -7,86 +7,86 @@ import { Component } from '@angular/core';
 })
 export class CellPhonePatientsComponent {
 
-  title = 'Cell Phone Patients'
-
+  title = 'Cell Phone Patients';
 
   phones =
     [
       {
         phoneName: 'iPhone 8+',
         repairPrice: `Repair Price - US $${75.00}`,
-        oldRepairPrice: `Repair Price - US ç$${90.00}`,
-        src: '../../assets/cellphone-images/cellphone-images/iphone8-plus-gold-select-2018.jpeg',
+        oldRepairPrice: `Repair Price - US $${90.00}`,
+        src: 'url(https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone8/plus/iphone8-plus-gold-select-2018?wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1522347727972)',
       },
       {
 
         phoneName: 'iPhone 8',
         repairPrice: `Repair Price - US $${70.00}`,
         oldRepairPrice: `Repair Price - US $${85.00}`,
-        src: '../../assets/cellphone-images/cellphone-images/iphone8.jpeg'
+        src: 'url(https://store.storeimages.cdn-apple.com/4981/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone8/plus/iphone8-plus-spgray-select-2018?wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1522347735544)',
       },
       {
 
         phoneName: 'iPhone 7+',
         repairPrice: `Repair Price - US $${75.00}`,
         oldRepairPrice: `Repair Price - US $${90.00}`,
-        src: '../../assets/cellphone-images/cellphone-images/iphone7plus.jpeg'
+        src: 'url(https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone7/plus/iphone7-plus-silver-select-2016?wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1472430173129)'
       },
       {
 
         phoneName: 'iPhone 7',
         repairPrice: `Repair Price - US $${70.00}`,
         oldRepairPrice: `Repair Price - US $${85.00}`,
-        src: '../../assets/cellphone-images/cellphone-images/iphone7.jpeg'
+        src: 'url(https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone7/plus/iphone7-plus-gold-select-2016?wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1472430108712)'
       },
       {
 
         phoneName: 'iPhone 6s+',
         repairPrice: `Repair Price - US $${65.00}`,
         oldRepairPrice: `Repair Price - US $${80.00}`,
-        src: '../../assets/cellphone-images/cellphone-images/iphone 6splus.jpeg'
+        src: 'url(https://store.storeimages.cdn-apple.com/4981/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone7/plus/iphone7-plus-rosegold-select-2016?wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1472430147951)'
       },
       {
 
         phoneName: 'iPhone 6s',
         repairPrice: `Repair Price - US $${60.00}`,
         oldRepairPrice: `Repair Price - US $${75.00}`,
-        src: '../../assets/cellphone-images/cellphone-images/iphone 6s.jpeg'
+        src: 'url(https://store.storeimages.cdn-apple.com/4981/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone7/plus/iphone7-plus-black-select-2016?wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1472430090682)'
       },
       {
 
         phoneName: 'iPhone 6+',
         repairPrice: `Repair Price - US $${65.00}`,
         oldRepairPrice: `Repair Price - US $${80.00}`,
-        src: '../../assets/cellphone-images/cellphone-images/iphone 6plus.jpeg'
+        src: 'url(https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone7/plus/iphone7-plus-silver-select-2016?wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1472430173129)'
       },
       {
 
         phoneName: 'iPhone 6',
         repairPrice: `Repair Price - US $${55.00}`,
         oldRepairPrice: `Repair Price - US $${70.00}`,
-        src: '../../assets/cellphone-images/cellphone-images/iphone six.jpeg'
+        src: 'url(https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone7/plus/iphone7-plus-gold-select-2016?wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1472430108712)'
       },
       {
 
         phoneName: 'iPhone SE',
         repairPrice: `Repair Price - US $${50.00}`,
         oldRepairPrice: `Repair Price - US $${65.00}`,
-        src: '../../assets/cellphone-images/cellphone-images/iphone SE.jpg'
+        src: 'url(https://images-na.ssl-images-amazon.com/images/I/61nFOTQPzEL._SX569_wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1472430108712.jpg)'
       },
       {
 
         phoneName: 'iPhone X, XR, XS',
         repairPrice: `Repair Price - US $${135.00}`,
         oldRepairPrice: `Repair Price - US $${165.00}`,
-        src: '../../assets/cellphone-images/cellphone-images/iphonexr.jpg'
+        src: 'url(https://i.gadgets360cdn.com/products/large/1536782796_635_iphone_xr.jpg',
+      
       },
       {
 
         phoneName: 'iPhone XS Max',
         repairPrice: `Repair Price - US $${185.00}`,
         oldRepairPrice: `Repair Price - US $${200.00}`,
-        src: '../../assets/cellphone-images/cellphone-images/iphoneXSMax.jpg'
+        src: '../../assets/cellphone-images/cellphone-images/iphoneXSMax'
       },
 
     ]

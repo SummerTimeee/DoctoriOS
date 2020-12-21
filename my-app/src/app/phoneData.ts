bc22dce7269272674cbb133fc44ecb8f9164b61e
Summer <Summerrichards01@gmail.com>
2020-12-20 21:17:19 -0600

    updating countdown timer and promo for the month

--- my-app/src/app/phoneData.ts
+++ my-app/src/app/phoneData.ts
@@ -3,35 +3,35 @@ export const phones: any = {
   'phones': [
     {
       phoneName: 'iPhone 8+',
-      repairPrice: `Repair Price - US $${75.00}`,
+      repairPrice: `Repair Price - US $${60.00}`,
       oldRepairPrice: `Repair Price - US $${90.00}`,
       src: 'url(https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone8/plus/iphone8-plus-gold-select-2018?wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1522347727972)',
     },
     {
 
       phoneName: 'iPhone 8',
-      repairPrice: `Repair Price - US $${70.00}`,
+      repairPrice: `Repair Price - US $${60.00}`,
       oldRepairPrice: `Repair Price - US $${85.00}`,
       src: 'url(https://store.storeimages.cdn-apple.com/4981/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone8/plus/iphone8-plus-spgray-select-2018?wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1522347735544)',
     },
     {
 
       phoneName: 'iPhone 7+',
-      repairPrice: `Repair Price - US $${75.00}`,
+      repairPrice: `Repair Price - US $${60.00}`,
       oldRepairPrice: `Repair Price - US $${90.00}`,
       src: 'url(https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone7/plus/iphone7-plus-silver-select-2016?wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1472430173129)'
     },
     {
 
       phoneName: 'iPhone 7',
-      repairPrice: `Repair Price - US $${70.00}`,
+      repairPrice: `Repair Price - US $${60.00}`,
       oldRepairPrice: `Repair Price - US $${85.00}`,
       src: 'url(https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone7/plus/iphone7-plus-gold-select-2016?wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1472430108712)'
     },
     {
 
       phoneName: 'iPhone 6s+',
-      repairPrice: `Repair Price - US $${65.00}`,
+      repairPrice: `Repair Price - US $${60.00}`,
       oldRepairPrice: `Repair Price - US $${80.00}`,
       src: 'url(https://store.storeimages.cdn-apple.com/4981/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone7/plus/iphone7-plus-rosegold-select-2016?wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1472430147951)'
     },
@@ -45,14 +45,14 @@ export const phones: any = {
     {
 
       phoneName: 'iPhone 6+',
-      repairPrice: `Repair Price - US $${65.00}`,
+      repairPrice: `Repair Price - US $${60.00}`,
       oldRepairPrice: `Repair Price - US $${80.00}`,
       src: 'url(https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone7/plus/iphone7-plus-silver-select-2016?wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1472430173129)'
     },
     {
 
       phoneName: 'iPhone 6',
-      repairPrice: `Repair Price - US $${55.00}`,
+      repairPrice: `Repair Price - US $${50.00}`,
       oldRepairPrice: `Repair Price - US $${70.00}`,
       src: 'url(https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone7/plus/iphone7-plus-gold-select-2016?wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1472430108712)'
     },
@@ -66,7 +66,7 @@ export const phones: any = {
     {
 
       phoneName: 'iPhone X, XR, XS',
-      repairPrice: `Repair Price - US $${135.00}`,
+      repairPrice: `Repair Price - US $${120.00}`,
       oldRepairPrice: `Repair Price - US $${165.00}`,
       src: 'url(https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-xr-blue-select-201809?wid=441&hei=529&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1565209264208)',
 
@@ -74,7 +74,7 @@ export const phones: any = {
     {
 
       phoneName: 'iPhone XS Max',
-      repairPrice: `Repair Price - US $${185.00}`,
+      repairPrice: `Repair Price - US $${120.00}`,
       oldRepairPrice: `Repair Price - US $${200.00}`,
       src: 'url(https://img.gkbcdn.com/p/2019-11-22/apple-iphone-x-64gb-silver--used--20191122085356800._w500_.jpg)'
     },

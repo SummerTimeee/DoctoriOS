903028ef9b582668c5bf4d04337a6aeeaac20344
Summer <Summerrichards01@gmail.com>
2021-01-01 11:40:41 -0600

    updating countdown timer

--- my-app/src/app/home/countdown-timer/countdown-timer.component.ts
+++ my-app/src/app/home/countdown-timer/countdown-timer.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./countdown-timer.component.css']
 })
 export class CountdownTimerComponent implements OnInit {
-  countDown = new Date('Dec 31, 2020 12:00:00').getTime();
+  countDown = new Date('Jan 31, 2021 12:00:00').getTime();
   timer = '';
   title = 'Hurry! Sale Ends:'
 

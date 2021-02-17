048aca270cf07d299bf64134b72b017f1d0c555f
Mercedes <Summerrichards01@gmail.com>
2021-02-17 15:57:27 -0600

    changing repair prices

--- my-app/dist/my-app/main-es5.js
+++ my-app/dist/my-app/main-es5.js
@@ -2471,57 +2471,57 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
     var phones = {
       'phones': [{
         phoneName: 'iPhone 8+',
-        repairPrice: "Repair Price - US $".concat(60.00),
+        repairPrice: "Repair Price - US $".concat(74.99),
         oldRepairPrice: "Repair Price - US $".concat(90.00),
         src: 'url(https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone8/plus/iphone8-plus-gold-select-2018?wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1522347727972)'
       }, {
         phoneName: 'iPhone 8',
-        repairPrice: "Repair Price - US $".concat(60.00),
+        repairPrice: "Repair Price - US $".concat(69.99),
         oldRepairPrice: "Repair Price - US $".concat(85.00),
         src: 'url(https://store.storeimages.cdn-apple.com/4981/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone8/plus/iphone8-plus-spgray-select-2018?wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1522347735544)'
       }, {
         phoneName: 'iPhone 7+',
-        repairPrice: "Repair Price - US $".concat(60.00),
+        repairPrice: "Repair Price - US $".concat(74.99),
         oldRepairPrice: "Repair Price - US $".concat(90.00),
         src: 'url(https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone7/plus/iphone7-plus-silver-select-2016?wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1472430173129)'
       }, {
         phoneName: 'iPhone 7',
-        repairPrice: "Repair Price - US $".concat(60.00),
+        repairPrice: "Repair Price - US $".concat(69.99),
         oldRepairPrice: "Repair Price - US $".concat(85.00),
         src: 'url(https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone7/plus/iphone7-plus-gold-select-2016?wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1472430108712)'
       }, {
         phoneName: 'iPhone 6s+',
-        repairPrice: "Repair Price - US $".concat(60.00),
+        repairPrice: "Repair Price - US $".concat(64.99),
         oldRepairPrice: "Repair Price - US $".concat(80.00),
         src: 'url(https://store.storeimages.cdn-apple.com/4981/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone7/plus/iphone7-plus-rosegold-select-2016?wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1472430147951)'
       }, {
         phoneName: 'iPhone 6s',
-        repairPrice: "Repair Price - US $".concat(60.00),
+        repairPrice: "Repair Price - US $".concat(59.99),
         oldRepairPrice: "Repair Price - US $".concat(75.00),
         src: 'url(https://store.storeimages.cdn-apple.com/4981/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone7/plus/iphone7-plus-black-select-2016?wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1472430090682)'
       }, {
         phoneName: 'iPhone 6+',
-        repairPrice: "Repair Price - US $".concat(60.00),
+        repairPrice: "Repair Price - US $".concat(59.99),
         oldRepairPrice: "Repair Price - US $".concat(80.00),
         src: 'url(https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone7/plus/iphone7-plus-silver-select-2016?wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1472430173129)'
       }, {
         phoneName: 'iPhone 6',
-        repairPrice: "Repair Price - US $".concat(50.00),
+        repairPrice: "Repair Price - US $".concat(54.99),
         oldRepairPrice: "Repair Price - US $".concat(70.00),
         src: 'url(https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/image/AppleInc/aos/published/images/i/ph/iphone7/plus/iphone7-plus-gold-select-2016?wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1472430108712)'
       }, {
         phoneName: 'iPhone SE',
-        repairPrice: "Repair Price - US $".concat(50.00),
+        repairPrice: "Repair Price - US $".concat(39.99),
         oldRepairPrice: "Repair Price - US $".concat(65.00),
         src: 'url(https://images-na.ssl-images-amazon.com/images/I/61nFOTQPzEL._SX569_wid=513&hei=556&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1472430108712.jpg)'
       }, {
         phoneName: 'iPhone X, XR, XS',
-        repairPrice: "Repair Price - US $".concat(120.00),
+        repairPrice: "Repair Price - US $".concat(134.99),
         oldRepairPrice: "Repair Price - US $".concat(165.00),
         src: 'url(https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-xr-blue-select-201809?wid=441&hei=529&fmt=jpeg&qlt=95&op_usm=0.5,0.5&.v=1565209264208)'
       }, {
         phoneName: 'iPhone XS Max',
-        repairPrice: "Repair Price - US $".concat(120.00),
+        repairPrice: "Repair Price - US $".concat(184.99),
         oldRepairPrice: "Repair Price - US $".concat(200.00),
         src: 'url(https://img.gkbcdn.com/p/2019-11-22/apple-iphone-x-64gb-silver--used--20191122085356800._w500_.jpg)'
       }]
